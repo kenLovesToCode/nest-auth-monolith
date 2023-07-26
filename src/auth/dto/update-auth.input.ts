9d7364f7dab303d519de3248ab40024905c4f371
Jovanne Vicentino <jvicentino@contractors.stevensonsystems.com>
2023-07-26 17:43:11 +0800

    cleanup code

--- src/auth/dto/update-auth.input.ts
+++
@@ -1,8 +0,0 @@
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-import { SignUpInput } from './input/sign-up.input';
-
-@InputType()
-export class UpdateAuthInput extends PartialType(SignUpInput) {
-  @Field(() => Int)
-  id: number;
-}
